681bf6daefde8a06935efbd72bab91c58083e87f
caoyao <caoyao@testbird.com>
2019-12-03 10:55:11 +0800

    add footer

--- components/end_footer/index.vue
+++ components/end_footer/index.vue
@@ -4,7 +4,7 @@
             <div class="margin_top_20">
                 <img src="../../static/img/logo2.png" class="width_30" alt="">
             </div>
-            <div class="font_size_14 margin_top_10 margin_bottom_15">Discover the World of Win-win</div>
+            <div class="font_size_14 margin_top_10 margin_bottom_15">Discover the World of Win-Win</div>
             <div class="border margin_auto">
             </div>
         </div>
